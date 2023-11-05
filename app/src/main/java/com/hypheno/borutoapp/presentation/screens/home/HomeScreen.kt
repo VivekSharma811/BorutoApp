ab758b689e6bfb3847b504ed8d0614b2e41890e5
Vivek <vivek.sharma@mpokket.com>
2023-11-05 18:06:43 +0530

    HomeTopBar added

--- app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.hypheno.borutoapp.presentation.screens.home
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            HomeTopBar(onSearchClicked = {})
+        }
+    ) {}
 }
---
+++ app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeTopBar.kt
@@ -0,0 +1,39 @@
+package com.hypheno.borutoapp.presentation.screens.home
+
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.hypheno.borutoapp.R
+import com.hypheno.borutoapp.ui.theme.topAppBarBackgroundColor
+import com.hypheno.borutoapp.ui.theme.topAppBarContentColor
+
+@Composable
+fun HomeTopBar(onSearchClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Explore",
+                color = MaterialTheme.colors.topAppBarContentColor
+            )
+        },
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        actions = {
+            IconButton(onClick = onSearchClicked) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search_icon)
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview
+fun HomeTopBarPreview() {
+    HomeTopBar {}
+}
