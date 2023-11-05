8ce4c644a44253a8d06db61d59044f7db5ebe3a8
Vivek <vivek.sharma@mpokket.com>
2023-11-05 13:43:08 +0530

    WelcomeScreen added

--- app/src/main/java/com/hypheno/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/hypheno/borutoapp/navigation/NavGraph.kt
@@ -6,20 +6,23 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.google.accompanist.pager.ExperimentalPagerApi
 import com.hypheno.borutoapp.presentation.screens.splash.SplashScreen
+import com.hypheno.borutoapp.presentation.screens.welcome.WelcomeScreen
 import com.hypheno.borutoapp.util.Constants.DETAILS_ARGUMENT_KEY
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Splash.route
+        startDestination = Screen.Welcome.route
     ) {
         composable(route = Screen.Splash.route) {
             SplashScreen(navController = navController)
         }
         composable(route = Screen.Welcome.route) {
-
+            WelcomeScreen(navController = navController)
         }
         composable(route = Screen.Home.route) {
 
