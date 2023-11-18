b29171891c723335cdd180ad809da4eb84c63a1e
Vivek <vivek.sharma@mpokket.com>
2023-11-18 13:01:27 +0530

    RemoteDataSourceImpl added

---
+++ app/src/main/java/com/hypheno/borutoapp/data/repository/RemoteDataSourceImpl.kt
@@ -0,0 +1,38 @@
+package com.hypheno.borutoapp.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.hypheno.borutoapp.data.local.BorutoDatabase
+import com.hypheno.borutoapp.data.paging_source.HeroRemoteMediator
+import com.hypheno.borutoapp.data.remote.BorutoApi
+import com.hypheno.borutoapp.domain.model.Hero
+import com.hypheno.borutoapp.domain.repository.RemoteDataSource
+import com.hypheno.borutoapp.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+
+@ExperimentalPagingApi
+class RemoteDataSourceImpl(
+    private val borutoApi: BorutoApi,
+    private val borutoDatabase: BorutoDatabase
+) : RemoteDataSource {
+
+    private val heroDao = borutoDatabase.heroDao()
+
+    override fun getAllHeroes(): Flow<PagingData<Hero>> {
+        val pagingSourceFactory = { heroDao.getAllHeroes() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = HeroRemoteMediator(
+                borutoApi = borutoApi,
+                borutoDatabase = borutoDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+    override fun searchHeroes(): Flow<PagingData<Hero>> {
+        TODO("Not yet implemented")
+    }
+}
