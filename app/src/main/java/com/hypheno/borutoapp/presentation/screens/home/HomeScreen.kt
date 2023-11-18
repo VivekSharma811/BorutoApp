dcbbd0cc322ec878d3c25c202fef02b5b1d1d0c5
Vivek <vivek.sharma@mpokket.com>
2023-11-18 13:17:23 +0530

    RatingWidget added

--- app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.hypheno.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.hypheno.borutoapp.presentation.components.RatingWidget
+import com.hypheno.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -15,5 +19,10 @@ fun HomeScreen(
         topBar = {
             HomeTopBar(onSearchClicked = {})
         }
-    ) {}
+    ) {
+        RatingWidget(
+            modifier = Modifier.padding(all = LARGE_PADDING),
+            rating = 4.4
+        )
+    }
 }
