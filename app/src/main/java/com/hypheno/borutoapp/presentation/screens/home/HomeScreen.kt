0008cc6f26cbddc1f4d961efec54e3c1652c51c7
Vivek <vivek.sharma@mpokket.com>
2023-11-18 13:47:55 +0530

    HeroItem added

--- app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/hypheno/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -5,12 +5,15 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.hypheno.borutoapp.presentation.common.ListContent
 import com.hypheno.borutoapp.presentation.components.RatingWidget
 import com.hypheno.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
+    navController: NavHostController,
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val allHeroes = homeViewModel.getAllHeroes.collectAsLazyPagingItems()
@@ -18,11 +21,12 @@ fun HomeScreen(
     Scaffold(
         topBar = {
             HomeTopBar(onSearchClicked = {})
+        },
+        content = {
+            ListContent(
+                heroes = allHeroes,
+                navController = navController
+            )
         }
-    ) {
-        RatingWidget(
-            modifier = Modifier.padding(all = LARGE_PADDING),
-            rating = 4.4
-        )
-    }
+    )
 }
